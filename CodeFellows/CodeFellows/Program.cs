1277106f466c77ef77cbf97bfe9873b551cd5005
Ariel R. Pedraza <pedrazaa2@gmail.com>
2017-12-28 09:39:26 -0800

    scaffold project

---
+++ CodeFellows/CodeFellows/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeFellows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
