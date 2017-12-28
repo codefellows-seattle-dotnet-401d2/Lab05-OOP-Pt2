c81cbdbe8d4c4b253e6f18d6a71db77fb2d55974
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-12-27 23:17:58 -0800

    Why do you no push?

---
+++ SchoolBuilder/SchoolBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
